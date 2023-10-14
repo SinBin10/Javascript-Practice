3e9e2121e455bff374f547d51a6f5ea78332f2ae
SinBin10 <bincode10@gmail.com>
2023-10-14 22:21:31 +0530

    Event Delegation Completed.

--- Event Delegation/app.js
+++ Event Delegation/app.js
@@ -1,5 +1,11 @@
 document.querySelector('#game').addEventListener('click', function(e){
     const color = e.target;
     e.target.style.backgroundColor = 'lightgrey';
-    console.log(e.target.getAttribute('Id') + ' is clicked');
+    console.log(e.target.getAttribute('id') + ' is clicked');
 });
+
+const div = document.createElement('div');
+const sport = document.querySelector('#game');
+div.innerText = 'Cricket';
+div.setAttribute('id', 'cricket');
+sport.appendChild(div);
