b96845074b622d6dc47b46125d57ec03365f56a5
SinBin10 <bincode10@gmail.com>
2023-10-18 10:23:15 +0530

    Final changes in modal.

--- Projects Javascript/Modal/app.js
+++ Projects Javascript/Modal/app.js
@@ -1,24 +1,20 @@
 //variables
 
 let btn = document.getElementById("modal-button");
-let modalContainer = document.getElementById("modal-container");
 let closebtn = document.getElementById("close-btn");
 let backdrop = document.querySelector('.backdrop');
 //Event listenrers
-btn.addEventListener("click", function (e) {
-  modalContainer.style.display = "flex";
+btn.addEventListener("click", function () {
   backdrop.style.display = "block";
 });
 
 closebtn.addEventListener("click", function () {
-  modalContainer.style.display = "none";
-  backdrop.style.display = "none";
+  backdrop.style.display = "none";  
 });
 
 window.addEventListener("click", function (e) {
     console.log(e.target);
   if (e.target === backdrop){ 
-  modalContainer.style.display = "none";
   backdrop.style.display = "none";
   }
 });
